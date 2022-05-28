b8195b3bec7a9bf0cc2d8c768fb9287498a99194
Shoaib Iqbal <shoaibiqbal0@gmail.com>
2022-05-28 22:10:38 +0100

    Styled Navbar and added menu

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
 import { FaBars, FaTimes } from "react-icons/fa";
 import Logo from "../assets/logo.png";
 
 function Navbar() {
+  const [nav, setNav] = useState(false);
+
+  const handleClick = () => setNav(!nav);
+
   return (
     <div className="fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0A192F] text-gray-300">
       <div>
         <img src={Logo} alt="Logo Image" style={{ width: "50px" }} />
       </div>
+      {/* Menu */}
+
+      <ul className="hidden md:flex">
+        <li>Home</li>
+        <li>About</li>
+        <li>Skills</li>
+        <li>Projects</li>
+        <li>Contact</li>
+      </ul>
+
+      {/* Hamburger */}
+      <div onClick={handleClick} className="md:hidden z-10">
+        {nav ? <FaTimes /> : <FaBars />}
+      </div>
+      {/* Mobile Menu */}
+      <ul
+        className={
+          nav
+            ? "absolute top-0 left-0 w-full h-screen bg-[#0A192F] flex flex-col justify-center items-center"
+            : "hidden"
+        }
+      >
+        <li className="py-6 text-4xl">Home</li>
+        <li className="py-6 text-4xl">About</li>
+        <li className="py-6 text-4xl">Skills</li>
+        <li className="py-6 text-4xl">Projects</li>
+        <li className="py-6 text-4xl">Contact</li>
+      </ul>
+      {/* Social Icons */}
+      <div className="hidden"></div>
     </div>
   );
 }
